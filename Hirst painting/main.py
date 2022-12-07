a2e7c5e9db4137cbb924e24de6b7ceba949eca71
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-07 10:56:24 +0200

    Create main.py

---
+++ Hirst painting/main.py
@@ -0,0 +1,29 @@
+import turtle
+import random
+
+turtle.colormode(255)
+tim = turtle.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+colors_list = [(226, 231, 236), (58, 106, 148), (224, 200, 109), (134, 84, 58), (223, 138, 62), (196, 145, 171), (234, 226, 204), (224, 234, 230), (141, 178, 204), (139, 82, 105), (209, 90, 69), (188, 80, 120), (68, 105, 90), (237, 225, 233), (134, 182, 136), (133, 133, 74), (63, 156, 92), (48, 156, 194), (183, 192, 201), (214, 177, 191), (19, 57, 93), (21, 68, 113), (112, 123, 149), (229, 174, 165), (172, 203, 182), (158, 205, 215), (69, 58, 47), (108, 47, 60), (53, 70, 65), (72, 64, 53)]
+
+tim.setheading(225)
+tim.forward(250)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+  tim.dot(20, random.choice(colors_list))
+  tim.forward(50)
+
+  if dot_count % 10 == 0:
+    tim.setheading(90)
+    tim.forward(50)
+    tim.setheading(180)
+    tim.forward(500)
+    tim.setheading(0)
+
+screen = turtle.Screen()
+screen.exitonclick()
