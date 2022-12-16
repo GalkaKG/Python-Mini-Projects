c05fd9251026b614214e0e9c0ad0b14554759419
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2022-12-16 16:46:41 +0200

    Update main.py

--- oop quiz game/main.py
+++ oop quiz game/main.py
@@ -1,21 +1,18 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
-
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
 
-while quiz.still_have_questions():
-    quiz.next_question()
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
 print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}{quiz.question_number}")
-
-
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
